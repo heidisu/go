be64a6394e3587fd6b10e2b74cbf269cf4c0dc9d
heidisu <heidimork@yahoo.no>
2018-02-10 14:13:12 +0100

    Update

--- FSNetCore/src/App/Domain.fs
+++ FSNetCore/src/App/Domain.fs
@@ -1,4 +1,5 @@
 module Domain
+    open System
 
     type Color = Black | White
 
@@ -14,19 +15,23 @@ module Domain
         col: int
     }
 
-    let neighbors point = 
+    let neighbors point size = 
+        let isOnGrid point = 
+            1 <= point.row  
+            && point.row <= size 
+            && 1 <= point.col 
+            && point.col <= size
         [
             { point with row = point.row - 1 }
             { point with row = point.row + 1 }
             { point with row = point.col - 1 }
             { point with row = point.col + 1 }
-        ]
+        ] |> Seq.filter isOnGrid
 
     type Move = 
     | Play of Point
     | Pass
     | Resign    
-
 
     type Stone = Stone of Point
 
@@ -75,14 +80,8 @@ module Domain
         grid
         |> Seq.tryFind (fun (p, _) -> p = point) 
         |> Option.map (fun (_, sg) -> sg)
-        
-    let isOnGrid board point = 
-        1 <= point.row  
-        && point.row <= board.size 
-        && 1 <= point.col 
-        && point.col <= board.size
     
-    let removeStoneGroup grid stoneGroup = 
+    let removeStoneGroup grid stoneGroup size = 
         let mutable newGrid = grid
         let updateNeighborStoneGroup nb =
             let neighborStoneGroup = getStoneGroup grid nb
@@ -94,7 +93,7 @@ module Domain
             | None -> ()
 
         for (Stone p ) in stoneGroup.stones do
-            for neighbor in (neighbors p) do
+            for neighbor in (neighbors p size) do
                 updateNeighborStoneGroup neighbor
             newGrid <- (removePoint grid p)
         newGrid
@@ -105,16 +104,14 @@ module Domain
         let mutable  adjacentOppositeColor = Set.empty
         let mutable liberties = Set.empty
         let mutable grid = board.grid
-        for nb in (neighbors point) do
-            if (isOnGrid board point)
-            then 
-                let neighborStoneGroup = getStoneGroup grid nb
-                match neighborStoneGroup with
-                | Some sg -> 
-                    if (sg.color = col) 
-                    then adjacentSameColor <- (adjacentSameColor.Add sg) 
-                    else adjacentOppositeColor <- (adjacentOppositeColor.Add sg)
-                | None -> liberties <- (liberties |> Set.add (Liberty nb))
+        for nb in (neighbors point board.size) do
+            let neighborStoneGroup = getStoneGroup grid nb
+            match neighborStoneGroup with
+            | Some sg -> 
+                if (sg.color = col) 
+                then adjacentSameColor <- (adjacentSameColor.Add sg) 
+                else adjacentOppositeColor <- (adjacentOppositeColor.Add sg)
+            | None -> liberties <- (liberties |> Set.add (Liberty nb))
         let mutable newStoneGroup = { 
                                 color = col
                                 stones = Set.singleton (Stone point)
@@ -128,9 +125,124 @@ module Domain
                                             |> Set.map (fun sg -> removeLiberty sg point)
         for sg in oppositeColorRemovedLiberties do
             if Set.count sg.liberties = 0
-            then grid <- removeStoneGroup grid sg
+            then grid <- removeStoneGroup grid sg board.size
         { board with grid = grid }
 
+    type GameState = {
+        board: Board
+        nextPlayer : Player
+        previousState : Option<GameState>
+        lastMove : Option<Move>
+    }
+
+    let applyMove gameState player move = 
+        if player <> gameState.nextPlayer then raise (Exception "fail")
+        match move with
+        | Play p -> let nextBoard = placeStone gameState.board player p
+                    { 
+                        board = nextBoard 
+                        nextPlayer = other player
+                        previousState = Some gameState
+                        lastMove = Some move
+                    }
+        | _ -> { 
+                    board = gameState.board
+                    nextPlayer = other player
+                    previousState = Some gameState
+                    lastMove = Some move
+                }
+
+    let newGame boardSize = {
+        board = { 
+                    size = boardSize 
+                    grid = Seq.empty
+                }
+        nextPlayer = Player Black
+        previousState = None
+        lastMove = None
+    }         
+
+    let isOver gameState = 
+        match gameState.lastMove with
+        | None -> false
+        | Some mv -> let secondLast = 
+                        match gameState.previousState with 
+                        | None -> None
+                        | Some gs -> gs.lastMove
+                     if secondLast = None then false
+                     else mv = Pass && secondLast = Some Pass
+
+    let isMoveSelfCapture gameState player move =
+        match move with
+        | Play pt -> let nextBoard = placeStone gameState.board player pt
+                     let nextStoneGroup = getStoneGroup nextBoard.grid pt
+                     match nextStoneGroup with
+                     | None -> false
+                     | Some sg -> sg.liberties.Count = 0
+        | _ -> false
+
+    let rec equalsPreviousState gameState player nextBoard =
+        match gameState with
+        | None -> false
+        | Some gs -> if gs.nextPlayer = other player && gs.board = nextBoard 
+                     then true
+                     else equalsPreviousState gs.previousState player nextBoard
+
+    let doesViolateKo gameState player move =
+        match move with
+        | Play pt -> let nextBoard = placeStone gameState.board player pt
+                     let pastState = gameState.previousState
+                     equalsPreviousState pastState player nextBoard
+        | _ -> false
 
 
+    let isValidMove gameState move = 
+        if isOver gameState then false
+        else match move with
+                | Play pt -> Option.isNone (getStoneGroup gameState.board.grid pt) &&
+                             not (isMoveSelfCapture gameState gameState.nextPlayer move) &&
+                             not (doesViolateKo gameState gameState.nextPlayer move)
+                | _ -> true
+
+    let pointIsEye board point (Player color) =
+        match getStoneGroup board.grid point with   
+        | None -> false
+        | Some _ -> let nbs = neighbors point board.size
+                    let unfriendlyCorners = nbs
+                                            |> Seq.filter (fun nb -> let nbSg = getStoneGroup board.grid nb
+                                                                     nbSg.IsSome && nbSg.Value.color <> color
+                                             )
+                                             |> Seq.length
+                    if unfriendlyCorners > 0
+                    then false 
+                    else
+                        let offBoardCorners = 4 - (Seq.length nbs)
+                        let friendlyCorners = 4 - offBoardCorners - unfriendlyCorners
+                        if offBoardCorners > 0 
+                        then offBoardCorners + friendlyCorners = 4
+                        else friendlyCorners >= 3
+                        
+    
+    let selectMove gameState =
+        let rnd = System.Random()
+        let candidates = 
+            seq { for i in 1 .. gameState.board.size 
+                    do for j in 1 .. gameState.board.size do yield (i, j)}
+            |> Seq.filter (fun (x, y) ->    let pt = {
+                                                        row = x
+                                                        col = y
+                                                     }
+                                            isValidMove gameState (Play pt) &&
+                                            not (pointIsEye gameState.board pt gameState.nextPlayer)
+            )
+
+        if Seq.isEmpty candidates
+        then Pass
+        else
+            let randomIndex = rnd.Next (Seq.length candidates - 1)
+            let random = Seq.item randomIndex candidates 
+            Play {
+                    row = (fst random)
+                    col = (snd random)
+                 }
 
--- FSNetCore/src/App/Program.fs
+++ FSNetCore/src/App/Program.fs
@@ -1,8 +1,45 @@
 ﻿// Learn more about F# at http://fsharp.org
-
+open Domain
 open System
+
+let cols = "ABCDEFGHIJKLMNOPQRST"
+let stoneToChar sg =
+    match sg with
+    | None -> "."
+    | Some sg -> if sg.color = Black then "x" else "o"
+
+let printMove player move = 
+    let moveString =
+        match move with
+        | Pass -> "passes"
+        | Resign -> "resigns"
+        | Play pt -> sprintf"%c%d" (cols |> Seq.item (pt.col-1)) pt.row
+    let playerString = 
+        match player with
+        | Player Black -> "black"
+        | Player White -> "white"
+    printfn "%s %s" playerString moveString
+
+let printBoard board =
+     for i in board.size .. -1 .. 1 do 
+        printf "%d " i
+        for j in 1 .. board.size do
+            let stoneGroup = getStoneGroup board.grid { row = i
+                                                        col = j}
+            printf "%s" (stoneToChar stoneGroup)
+        printfn ""
+     printfn "  %s" (cols |> Seq.take board.size |> String.Concat)
+
 
 [<EntryPoint>]
 let main argv =
+    let boardSize = 9
+    let mutable game = newGame boardSize
+
+    while not (isOver game) do
+        printBoard game.board
+        let move = Domain.selectMove game
+        printMove game.nextPlayer move
+        game <- Domain.applyMove game game.nextPlayer move 
     printfn "Hello World from F#!"
     0 // return an integer exit code
